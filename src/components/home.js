b73ef9d9ddc8e68d25b481add15bfcda3f5161f8
Izyk <lance_hemphill1234@yahoo.com>
2023-11-23 17:46:37 -0500

    finished navbar

---
+++ src/components/home.js
@@ -0,0 +1,8 @@
+
+const Home = () => {
+    return (
+        <div>Placeholder</div>
+    )
+}
+
+export default Home;
---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import '../styles/navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className='navContainer'>
+            <ul className='navbar'>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+                <li>Github</li>
+                <li>Resume</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;

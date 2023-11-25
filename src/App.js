da847a905293ef32b6c1a0fb507003ebaf3081a7
Izyk <lance_hemphill1234@yahoo.com>
2023-11-24 21:48:06 -0500

    restored App.js file that I accidentally deleted lol

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import AOS from 'aos';
+import "aos/dist/aos.css";
+import Home from './components/home';
+import Navbar from './components/navbar';
+
+
+const App = () => {
+
+  AOS.init({
+    duration: 1000
+  })
+
+  useEffect(() => {
+    AOS.init()
+  }, [])
+
+  return (
+    <div className="App">
+      <Navbar AOS={'data-aos'} />
+      <Home AOS={'data-aos'} />
+
+    </div>
+  );
+}
+
+export default App;

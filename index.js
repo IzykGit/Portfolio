3ad9b40a541345fbb95ffa7b5bb53545ed81a3d3
Izyk <lance_hemphill1234@yahoo.com>
2024-03-15 16:36:22 -0400

    finished second landing design

---
+++ index.js
@@ -0,0 +1,59 @@
+
+const navLinks = document.querySelectorAll('.navlink')
+
+navLinks.forEach(link => {
+    link.addEventListener("mouseover", () => {
+        const generateHoverVariable = Math.floor(Math.random() * 2)
+        console.log(generateHoverVariable)
+
+        if(generateHoverVariable === 0) {
+            link.classList.add('rotate-left')
+            console.log(link.classList)
+        }
+        else if (generateHoverVariable === 1){
+            link.classList.add('rotate-right')
+            console.log(link.classList)
+        }
+    })
+
+    link.addEventListener("mouseleave", () => {
+        link.classList.remove('rotate-left', 'rotate-right')
+
+        console.log("Mouse left", link.classList)
+    })
+})
+
+
+
+
+
+// calculating the scroll distance that allows the scroll percentage to move
+// translating the text along the X axis when scrolling
+
+
+const scrolled = document.querySelector('.scroll-bar');
+const scrollPercentText = document.querySelector('.scroll-percent');
+
+let scrollPercentage;
+
+function calculateScrollDistance() {
+    const scrollTop = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+  
+    const totalScrollableDistance = documentHeight - windowHeight;
+    scrollPercentage = (scrollTop / totalScrollableDistance) * 100;
+  
+    console.log(`Scrolled: ${scrollPercentage.toFixed(2)}%`);
+    console.log(scrollPercentage.toFixed(2))
+
+    if(scrollPercentage > 95) {
+        return
+    }
+    scrolled.style.transform = `translateX(${scrollPercentage.toFixed(2)}%)`
+}
+
+window.addEventListener('scroll', calculateScrollDistance);
+
+
+
